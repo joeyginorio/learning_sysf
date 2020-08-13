a191ba435791ef772aa37be9c7e860d75f50538c
joey <joeyvelez@knights.ucf.edu>
2020-08-13 19:33:18 -0400

    wrote small tester for generating polymorphic identity

--- Test.hs
+++ Test.hs
@@ -3,3 +3,17 @@ Testing.hs
 ==============================================================================
 Tests learning in System F for several terms. Useful for profiling performance.
 -}
+
+import Learning
+import SystemF
+
+genId :: Context -> Int -> [Term]
+genId ctx n =
+  let idTy = (TyTAbs "X" (TyAbs (TyVar "X") (TyVar "X")))
+      in genTerms idTy ctx n
+
+main = do
+  let ctx = []
+  let n   = 17
+  let tms = genId ctx n
+  print $ tms
