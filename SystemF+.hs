cff3bb7412db267da3927ba9d6e6535d88afeee6
joey <joeyvelez@knights.ucf.edu>
2020-09-02 20:06:58 -0400

    added systemf+.hs which will hold the sugared version of system f we parse to

---
+++ SystemF+.hs
@@ -0,0 +1,6 @@
+{-|
+SystemF+.hs
+==============================================================================
+Defines syntax, typing, evaluation for System F. Follows the specification in
+Chapter 2 of "Learning in System F".
+-}
