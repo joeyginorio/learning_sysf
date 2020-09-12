6a9102f9ba134a4cbb12252b52896a08027bda65
joey <joeyvelez@knights.ucf.edu>
2020-09-12 15:07:18 -0400

    definig syntax for sysf+, need to read up on how to handle pattern matching

--- SystemF+.hs
+++ SystemF+.hs
@@ -1,6 +1,46 @@
 {-|
 SystemF+.hs
-==============================================================================
-Defines syntax, typing, evaluation for System F. Follows the specification in
-Chapter 2 of "Learning in System F".
+===============================================================================
+Defines syntax, typing, evaluation for System F+. A sugared version of System F
+with sum/products + let statements.
 -}
+
+{-
+TODO
+- syntax
+- semantics (typechecker, eval)
+-}
+
+{- ====================== Syntax of Terms & Types  ==========================-}
+type Id = String
+type Constr = String
+
+data Term = TmUnit
+          | TmTrue
+          | TmFalse
+          | TmVar Id
+          | TmAbs Id Type Term
+          | TmApp Term Term
+          | TmTAbs Id Term
+          | TmTApp Term Type
+          | TmFst Term
+          | TmSnd Term
+          | TmProd Term Term
+          | TmInL Term
+          | TmInR Term
+          | TmSCase Term (Id,Term) (Id,Term)
+          | TmVCase Id [(Pattern, Term)]
+          deriving (Eq, Show)
+
+data Type = TyUnit
+          | TyBool
+          | TyAbs Type Type
+          | TyTAbs Id Type
+          | TyProd Type Type
+          | TySum Type Type
+          | TyVariant [(Constr, [Type])]
+          deriving (Eq, Show)
+
+data Pattern = 
+
+
