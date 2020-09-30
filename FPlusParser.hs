02ba135a37f5ef603cae2a7098b84c326b6256d4
joey <joeyvelez@knights.ucf.edu>
2020-09-30 17:21:41 -0400

    copy/pasta from parser of souped up STLC. works for subset of FPlus, need to flesh out parser for variants/lets

--- FPlusParser.hs
+++ FPlusParser.hs
@@ -45,23 +45,23 @@ type DDecl = (String, Type)
 
 {-================================ PARSE TYPES ===============================-}
 
-tyUnit' :: Parser Type
-tyUnit' = do symbol "Unit"
-             return TyUnit'
+tyUnit :: Parser Type
+tyUnit = do symbol "Unit"
+            return TyUnit
 
-tyBool' :: Parser Type
-tyBool' = do symbol "Bool"
-             return TyBool'
+tyBool :: Parser Type
+tyBool = do symbol "Bool"
+            return TyBool
 
-tyAbsOp' :: Parser (Type -> Type -> Type)
-tyAbsOp' = do symbol "->"
-              return TyAbs'
+tyAbsOp :: Parser (Type -> Type -> Type)
+tyAbsOp = do symbol "->"
+             return TyAbs
 
-tyAtom' :: Parser Type
-tyAtom' = tyUnit' <|> tyBool' <|> parens ty'
+tyAtom :: Parser Type
+tyAtom = tyUnit <|> tyBool <|> parens ty
 
-ty' :: Parser Type
-ty' = tyAtom' `chainr1` tyAbsOp'
+ty :: Parser Type
+ty = tyAtom `chainr1` tyAbsOp
 
 
 {-================================ PARSE TERMS ===============================-}
@@ -69,49 +69,49 @@ ty' = tyAtom' `chainr1` tyAbsOp'
 keywords :: [String]
 keywords = ["let", "in", "lam"]
 
-tmUnit' :: Parser Term'
-tmUnit' = do symbol "unit"
-             return TmUnit'
+tmUnit :: Parser Term
+tmUnit = do symbol "unit"
+            return TmUnit
 
-tmTrue' :: Parser Term'
-tmTrue' = do symbol "tt"
-             return TmTrue'
+tmTrue :: Parser Term
+tmTrue = do symbol "tt"
+            return TmTrue
 
-tmFalse' :: Parser Term'
-tmFalse' = do symbol "ff"
-              return TmFalse'
+tmFalse :: Parser Term
+tmFalse = do symbol "ff"
+             return TmFalse
 
-tmVar' :: Parser Term'
-tmVar' = do x <- identifier keywords
-            return $ TmVar' x
+tmVar :: Parser Term
+tmVar = do  x <- identifier keywords
+            return $ TmVar x
 
-tmAbs' :: Parser Term'
-tmAbs' = do symbol "lam"
+tmAbs :: Parser Term
+tmAbs = do  symbol "lam"
             x <- identifier keywords
             symbol ":"
-            a <- ty'
+            a <- ty
             symbol "."
-            t <- tm'
-            return $ TmAbs' x a t
+            t <- tm
+            return $ TmAbs x a t
 
-tmLet' :: Parser Term'
-tmLet' = do symbol "let"
-            x <- identifier keywords
-            symbol "="
-            t1 <- tm'
-            symbol "in"
-            t2 <- tm'
-            return $ TmLet' x t1 t2
+-- tmLet :: Parser Term
+-- tmLet = do  symbol "let"
+--             x <- identifier keywords
+--             symbol "="
+--             t1 <- tm
+--             symbol "in"
+--             t2 <- tm
+--             return $ TmLet x t1 t2
 
-tmAtom' :: Parser Term'
-tmAtom' = tmLet' <|> tmAbs' <|> tmUnit' <|> tmTrue' <|> tmFalse' <|> tmVar' <|>
-          parens tm'
+tmAtom :: Parser Term
+tmAtom = tmAbs <|> tmUnit <|> tmTrue <|> tmFalse <|> tmVar <|>
+          parens tm
 
-tmAppOp' :: Parser (Term' -> Term' -> Term')
-tmAppOp' = return TmApp'
+tmAppOp :: Parser (Term -> Term -> Term)
+tmAppOp = return TmApp
 
-tm' :: Parser Term'
-tm' = do tmAtom' `chainl1` tmAppOp'
+tm :: Parser Term
+tm = do tmAtom `chainl1` tmAppOp
 
 
 {-================================ PARSE DECLS ===============================-}
@@ -119,7 +119,7 @@ tydecl :: Parser TyDecl
 tydecl = do allSpace
             f <- identifier keywords
             symbol "::"
-            a <- ty'
+            a <- ty
             return (f,a)
 
 tmdecl :: Parser TmDecl
@@ -127,7 +127,7 @@ tmdecl = do allSpace
             f <- identifier keywords
             ps <- many (identifier keywords)
             symbol "="
-            t <- tm'
+            t <- tm
             return (f,ps,t)
 
 decl :: Parser Decl
@@ -150,54 +150,3 @@ foo = "\n\
 
 parseFile :: String -> Parser a -> IO a
 parseFile f p = fmap (fst . head . parse p) (readFile f)
-
-{-================================ DESUGAR PROGS =============================-}
-
-desugar :: Prog -> Term
-desugar p = desugarTm (desugarProg p)
-
-desugarProg :: Prog -> Term'
-desugarProg (d:[]) = desugarDecl d
-desugarProg (((_,typ),(f,ps,tm)):ds) =
-  let tm1 = desugarDecl ((f,typ),(f,ps,tm))
-      tm2 = desugarProg ds
-      in TmLet' f tm1 tm2
-
-desugarDecl :: Decl -> Term'
-desugarDecl ((_,typ), (f,[],tm)) = tm
-desugarDecl ((_,(TyAbs' typ1 typ2)), (f,(p:ps),tm)) =
-  TmAbs' p typ1 (desugarDecl ((f,typ2),(f,ps,tm)))
-
-
-desugarTm :: Term' -> Term
-desugarTm (TmUnit') = TmUnit
-desugarTm (TmTrue') = TmTrue
-desugarTm (TmFalse') = TmFalse
-desugarTm (TmVar' x) = TmVar x
-desugarTm (TmAbs' x a t) = TmAbs x (desugarTy a) (desugarTm t)
-desugarTm (TmApp' t1 t2) = TmApp (desugarTm t1) (desugarTm t2)
-desugarTm (TmLet' x t1 t2) = TmApp (TmAbs x a t2') t1'
-                             where a = desugarTy (typeOf' t1 [])
-                                   t1' = desugarTm t1
-                                   t2' = desugarTm t2
-
-desugarTy :: Type -> Type
-desugarTy (TyUnit') = TyUnit
-desugarTy (TyBool') = TyBool
-desugarTy (TyAbs' typ1 typ2) = (TyAbs (desugarTy typ1) (desugarTy typ2))
-
--- Typechecker for front-end AST
-type Context' = [(String,Type)]
-
-typeOf' :: Term' -> Context' -> Type
-typeOf' (TmUnit') _ = TyUnit'
-typeOf' (TmTrue') _ = TyBool'
-typeOf' (TmFalse') _ = TyBool'
-typeOf' (TmVar' x) ctx = snd (head ctx')
-                         where ctx' = filter (\(y,t) -> y == x) ctx
-typeOf' (TmAbs' x a t) ctx = TyAbs' a (typeOf' t ctx')
-                             where ctx' = ("x",a):ctx
-typeOf' (TmApp' t1 t2) ctx = ty12
-                             where TyAbs' ty11 ty12 = typeOf' t1 ctx
-typeOf' (TmLet' x t1 t2) ctx = typeOf' t2 ctx'
-                               where ctx' = ("x", typeOf' t1 ctx):ctx
