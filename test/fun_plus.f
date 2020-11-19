1e91574ec76eb05d531bcbe8506e4e37fdcfb7e0
Nada Amin <namin@seas.harvard.edu>
2020-11-18 21:05:44 -0500

    this one is fast

---
+++ test/fun_plus.f
@@ -0,0 +1,17 @@
+data Bool = True
+          | False
+
+data Nat = Zero
+         | S Nat
+
+inc0 :: Nat -> Nat
+inc0 = [<Zero,Zero>,<S Zero, S Zero>]
+
+inc1 :: Nat -> Nat
+inc1 = [<Zero,S Zero>,<S Zero, S (S Zero)>]
+
+pointwise :: (Nat -> Nat) -> (Nat -> Nat) -> (Nat -> Nat)
+pointwise = [<inc0,inc0,Zero,Zero>,<inc0,inc1,Zero, S Zero>,<inc1,inc0,Zero, S Zero>]
+
+main :: Unit
+main = pointwise inc1 inc1 Zero
