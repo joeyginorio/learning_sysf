936d75770274019880ba866e533608837d4bec60
Nada Amin <namin@seas.harvard.edu>
2020-11-20 21:53:53 -0500

    fix

--- test/data_fun.f
+++ test/data_fun.f
@@ -23,11 +23,24 @@ nat2bool :: Nat -> Bool
 nat2bool = [<Zero,False>,<S Zero,True>,<S (S Zero),True>]
 
 appFun :: Fun -> Data -> Data
-appFun = [<BoolFun (lam x:Bool.x),BoolData True,BoolData True>,
-          <BoolFun (lam x.Bool.x),NatData (S Zero),BoolData True>,
-          <NatFun inc, NatData (S Zero), NatData (S (S Zero))>,
-          <NatFun inc, BoolData False, NatData (S Zero)>]
+appFun = [<BoolFun (lam x:Bool.x),BoolData False,BoolData False>,
+          <BoolFun (lam x:Bool.x),BoolData True,BoolData True>,
+          <BoolFun (lam x:Bool.x),NatData Zero,BoolData False>,
+          <BoolFun (lam x:Bool.x),NatData (S Zero),BoolData True>,
+          <BoolFun (lam x:Bool.not x),BoolData False,BoolData True>,
+          <BoolFun (lam x:Bool.not x),BoolData True,BoolData False>,
+          <BoolFun (lam x:Bool.not x),NatData Zero,BoolData True>,
+          <BoolFun (lam x:Bool.not x),NatData (S Zero),BoolData False>,
+          <NatFun (lam x:Nat.x),BoolData False,NatData Zero>,
+          <NatFun (lam x:Nat.x),BoolData True,NatData (S Zero)>,
+          <NatFun (lam x:Nat.x),NatData Zero,NatData Zero>,
+          <NatFun (lam x:Nat.x),NatData (S Zero),NatData (S Zero)>,
+          <NatFun (lam x:Nat.inc x),BoolData False,NatData (S Zero)>,
+          <NatFun (lam x:Nat.inc x),BoolData True,NatData (S (S Zero))>,
+          <NatFun (lam x:Nat.inc x),NatData Zero,NatData (S Zero)>,
+          <NatFun (lam x:Nat.inc x),NatData (S Zero),NatData (S (S Zero))>]
+
 
 main :: Unit
-main = appFun (NatFun inc) (BoolData True)
+main = True
 
